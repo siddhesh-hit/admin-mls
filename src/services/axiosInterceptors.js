6b960d055254ca722692bd5885da782ffc8179e7
siddhesh-hit <siddhesh@handsintechnology.com>
2024-02-19 12:00:00 +0530

    modified: api name

--- src/services/axiosInterceptors.js
+++ src/services/axiosInterceptors.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:8484/v1/api",
-  // baseURL: "https://mlsapi.handsintechnology.in/v1/api",
-  baseURL: "https://mlsapi.sblcorp.com/v1/api",
-
+  // baseURL: "http://localhost:8484/api/v1/",
+  // baseURL: "https://mlsapi.handsintechnology.in/api/v1/",
+  baseURL: "https://mlsapi.sblcorp.com/api/v1/",
   withCredentials: true,
 });
 
@@ -31,9 +30,9 @@ axiosInstance.interceptors.response.use(
       try {
         const res = await axios
           .post(
-            // "http://localhost:8484/v1/api/user/accessToken",
-            // "https://mlsapi.handsintechnology.in/v1/api/accessToken",
-            "https://mlsapi.sblcorp.com/v1/api/user/accessToken",
+            // "http://localhost:8484/api/v1/user/accessToken",
+            // "https://mlsapi.handsintechnology.in/api/v1/accessToken",
+            "https://mlsapi.sblcorp.com/api/v1/user/accessToken",
             {},
             { withCredentials: true }
           )
